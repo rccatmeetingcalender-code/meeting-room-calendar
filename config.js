28b401a309cfbeff0c7304f8200551d8b603f3c5
rccatmeetingcalender-code <rccatmeetingcalender@gmail.com>
2025-11-03 21:46:40 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,2 @@
-const calendarUrl = "https://outlook.office365.com/owa/calendar/dbd6d7f5864642648419c3cba353581e@polyu.edu.hk/644bff1a9dd149eb966b3873bcbb76c211192125406565934742/calendar.ics";
+const calendarUrl = "https://www.calendarlabs.com/ical-calendar/ics/46/Germany_Holidays.ics";
+
